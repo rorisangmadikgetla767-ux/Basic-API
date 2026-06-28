8a5b704744e150e036df931fc97e0922a0e1105a
rorisangmadikgetla767-ux <rorisangmadikgetla767@gmail.com>
2026-06-28 19:20:33 +0200

    ...

--- auth/router.py
+++ auth/router.py
@@ -3,7 +3,7 @@ from database import get_connection
 from schemas import UserRegister, UserLogin, UserResponse, TokenResponse
 from auth.utils import hash_password, verify_password, create_access_token
 
-router = APIRouter
+router = APIRouter()
 @router.post("/register", response_model=UserResponse)
 def register(user: UserRegister):
     conn = get_connection()
@@ -26,7 +26,7 @@ def register(user: UserRegister):
         conn.rollback()
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Email already registered"
+            detail=str(e)
         )
     finally:
         cursor.close()
